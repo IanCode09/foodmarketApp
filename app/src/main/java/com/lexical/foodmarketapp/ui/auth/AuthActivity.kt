eeda0d7ee8c174bce96c8c0c6ed4c429aec90c6f
ian lombu <citrakemurnian@gmail.com>
2021-08-28 01:12:00 +0700

    Auth Navigation

---
+++ app/src/main/java/com/lexical/foodmarketapp/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.lexical.foodmarketapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lexical.foodmarketapp.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
