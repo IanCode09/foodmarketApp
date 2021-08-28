66b2582f75f0e56f947431a6ff4118feebc3b568
ian lombu <citrakemurnian@gmail.com>
2021-08-28 14:11:43 +0700

    Auth Navigation Part 3

--- app/src/main/java/com/lexical/foodmarketapp/ui/auth/signup/SignupAddressFragment.kt
+++ app/src/main/java/com/lexical/foodmarketapp/ui/auth/signup/SignupAddressFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.lexical.foodmarketapp.R
+import com.lexical.foodmarketapp.databinding.FragmentSignupAddressBinding
 
 class SignupAddressFragment : Fragment() {
+
+    private var _binding : FragmentSignupAddressBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup_address, container, false)
+
+        _binding = FragmentSignupAddressBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        binding.btnSignUpNow.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_signup_success, null)
+        }
     }
 }
--- app/src/main/java/com/lexical/foodmarketapp/ui/auth/signup/SignupFragment.kt
+++ app/src/main/java/com/lexical/foodmarketapp/ui/auth/signup/SignupFragment.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.lexical.foodmarketapp.R
+import com.lexical.foodmarketapp.databinding.FragmentSignupBinding
 
 class SignupFragment : Fragment() {
+
+    private var _binding : FragmentSignupBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+        _binding = FragmentSignupBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        binding.btnContinue.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_signup_address, null)
+        }
     }
 }
