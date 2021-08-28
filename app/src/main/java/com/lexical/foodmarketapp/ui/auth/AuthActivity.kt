e8a2d51e7cababa41a624e6ecdc004044d7b9673
ian lombu <citrakemurnian@gmail.com>
2021-08-28 13:53:34 +0700

    Auth Navigation Part 2

--- app/src/main/java/com/lexical/foodmarketapp/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/lexical/foodmarketapp/ui/auth/AuthActivity.kt
@@ -2,11 +2,21 @@ package com.lexical.foodmarketapp.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
 import com.lexical.foodmarketapp.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val pageRequest = intent.getIntExtra("page_request", 0)
+
+        if (pageRequest == 2) {
+            val navOptions = NavOptions.Builder().setPopUpTo(R.id.fragmentSignIn, true).build()
+
+            Navigation.findNavController(findViewById(R.id.authHostFragment)).navigate(R.id.action_signup, null, navOptions)
+        }
     }
 }
--- app/src/main/java/com/lexical/foodmarketapp/ui/auth/signin/SigninFragment.kt
+++ app/src/main/java/com/lexical/foodmarketapp/ui/auth/signin/SigninFragment.kt
@@ -1,19 +1,37 @@
 package com.lexical.foodmarketapp.ui.auth.signin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.lexical.foodmarketapp.R
+import com.lexical.foodmarketapp.databinding.FragmentSigninBinding
+import com.lexical.foodmarketapp.ui.auth.AuthActivity
 
 class SigninFragment : Fragment() {
+
+    private var _binding: FragmentSigninBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signin, container, false)
+        _binding = FragmentSigninBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        binding.btnSignup.setOnClickListener {
+            val signup = Intent(activity, AuthActivity::class.java)
+            signup.putExtra("page_request", 2)
+            startActivity(signup)
+        }
     }
 }
