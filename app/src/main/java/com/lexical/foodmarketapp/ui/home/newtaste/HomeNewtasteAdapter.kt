092d287a7cb4854b2ac7af4ed2566c7d7cce7f05
ian lombu <citrakemurnian@gmail.com>
2021-09-04 00:26:47 +0700

    Adapter Item Food Vertical & Fungsi Currency

---
+++ app/src/main/java/com/lexical/foodmarketapp/ui/home/newtaste/HomeNewtasteAdapter.kt
@@ -0,0 +1,57 @@
+package com.lexical.foodmarketapp.ui.home.newtaste
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.lexical.foodmarketapp.R
+import com.lexical.foodmarketapp.model.dummy.HomeModel
+import com.lexical.foodmarketapp.utils.Helpers.formatPrice
+import kotlinx.android.synthetic.main.fragment_home.view.*
+import kotlinx.android.synthetic.main.fragment_home.view.tvTitle
+import kotlinx.android.synthetic.main.item_home_horizontal.view.*
+import kotlinx.android.synthetic.main.item_home_horizontal.view.ivPoster
+import kotlinx.android.synthetic.main.item_home_horizontal.view.rbFood
+import kotlinx.android.synthetic.main.item_home_vertical.view.*
+
+class HomeNewtasteAdapter (
+        private val listData: List<HomeModel>,
+        private val itemAdapterCallback: ItemAdapterCallback
+    ) : RecyclerView.Adapter<HomeNewtasteAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeNewtasteAdapter.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.item_home_vertical, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(listData[position], itemAdapterCallback)
+    }
+
+    override fun getItemCount(): Int {
+        return listData.size
+    }
+
+    class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(data: HomeModel, itemAdapterCallback: ItemAdapterCallback) {
+            itemView.apply {
+                tvTitle.text = data.title
+                tvPrice.formatPrice(data.title)
+                rbFood.rating = data.rating
+
+//                Glide.with(context).load(data.src).into(ivPoster)
+
+                itemView.setOnClickListener {
+                    itemAdapterCallback.onClick(it, data)
+                }
+            }
+        }
+    }
+
+    interface ItemAdapterCallback {
+        fun onClick(v: View, data: HomeModel)
+    }
+}
