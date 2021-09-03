b572aa9b124775301bd12b7d9ed55b59c5355594
ian lombu <citrakemurnian@gmail.com>
2021-09-04 01:15:32 +0700

    Order Empty Page

--- app/src/main/java/com/lexical/foodmarketapp/ui/order/OrderFragment.kt
+++ app/src/main/java/com/lexical/foodmarketapp/ui/order/OrderFragment.kt
@@ -12,32 +12,18 @@ import com.lexical.foodmarketapp.databinding.FragmentOrderBinding
 
 class OrderFragment : Fragment() {
 
-  private lateinit var dashboardViewModel: DashboardViewModel
-private var _binding: FragmentOrderBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
-  private val binding get() = _binding!!
+    private var _binding: FragmentOrderBinding? = null
+    private val binding get() = _binding!!
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
-    _binding = FragmentOrderBinding.inflate(inflater, container, false)
-    val root: View = binding.root
-
-    val textView: TextView = binding.textDashboard
-    dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-      textView.text = it
-    })
-    return root
-  }
-
-override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        _binding = FragmentOrderBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
     }
+
 }
