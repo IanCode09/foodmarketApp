e54a726e2035ea0a849986d5c734163a83ea2a55
ian lombu <citrakemurnian@gmail.com>
2021-09-05 14:00:31 +0700

    Checkout Part 1

---
+++ app/src/main/java/com/lexical/foodmarketapp/ui/detail/PaymentFragment.kt
@@ -0,0 +1,28 @@
+package com.lexical.foodmarketapp.ui.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lexical.foodmarketapp.R
+import com.lexical.foodmarketapp.databinding.FragmentDetailBinding
+import com.lexical.foodmarketapp.databinding.FragmentPaymentBinding
+
+class PaymentFragment : Fragment() {
+
+    private var _binding: FragmentPaymentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentPaymentBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+}
